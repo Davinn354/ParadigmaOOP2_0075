0200c8bb5f05d752b32dd76412c024cd053f7c06
Davinn354 <daviipad53@gmail.com>
2024-05-27 14:54:39 +0700

    Penambahan include

--- noOveridding.cpp
+++ noOveridding.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class baseClass {
 public :
-    virtual void perkenalan() final {
+    virtual void perkenalan() /*final*/ {
         cout << "Hallo saya Function dari base class";
     }
 
@@ -14,3 +14,9 @@ public:
         cout << "Hallo saya function ddari derived Class ";
     }
 };
+int main() {
+    derivedClass a; // akses langsung pada class
+    a.perkenalan();
+
+    return 0;
+}
